13363a1eb02ab279ef0820222d20517241c89da2
Murali Shankar <mshankar_at_slac.stanford.edu>
2012-09-27 15:42:41 -0700

    Add a couple of -D's to make this faster across an SSH connection

--- run.sh
+++ run.sh
@@ -15,6 +15,8 @@ export CLASSPATH=${CLASSPATH}:ext_jars/protobuf-java-2.4.1.jar
 echo "Classpath is ${CLASSPATH}"
 
 java \
+-Dsun.java2d.pmoffscreen=false \
+-Dsun.java2d.print.polling=false \
 -Dlog4j.configuration=log4j.properties \
 -classpath ${CLASSPATH} \
 epics.archiveviewer.base.Launcher \
