b2dfd58104fa26cbe5645ad70cb262fedb313fa6
Murali Shankar <mshankar_at_slac.stanford.edu>
2014-03-14 14:13:31 -0700

    Add lastSample

--- src/epics/archiveviewer/xal/view/AVEsSelectorPanel.java
+++ src/epics/archiveviewer/xal/view/AVEsSelectorPanel.java
@@ -33,7 +33,7 @@ public class AVEsSelectorPanel extends AVAbstractPanel
         private JLabel densityLabel;
 	private JComboBox densitySelectionBox;
 
-	private String sparsificationOperators[] = {"firstSample", "lastFill", "mean", "median", "min", "max", "Raw"};
+	private String sparsificationOperators[] = {"lastFill", "firstSample", "lastSample", "mean", "median", "min", "max", "Raw"};
 
 	private JButton searchButton;
 	private JTextField inputField;
